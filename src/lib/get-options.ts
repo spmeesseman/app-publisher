3d2e2720042fc9d4aa93cad9240946a468c1f645
Scott Meesseman <spmeesseman@gmail.com>
2021-06-28 06:01:56 -0400

    progress: prepare for tests [skip ci]

---
+++ src/lib/get-options.ts
@@ -0,0 +1,56 @@
+import chalk from "chalk";
+import gradient from "gradient-string";
+import { publishRcOpts } from "../args";
+import { IOptions } from "../interface";
+
+export = getOptions;
+
+
+function getOptions(useBanner = true): IOptions
+{
+    const version = require("../../package.json").version,
+          banner = apBanner(version),
+          ArgParser = require("@spmeesseman/arg-parser").ArgParser;
+
+    const parser = new ArgParser({
+        app: "app-publisher",
+        banner, version,
+        ignorePositional: [ "-p", "--profile" ]
+    });
+    const opts = parser.parseArgs(publishRcOpts);
+
+    //
+    // Set task mode stdout flag on the options object
+    //
+    opts.taskModeStdOut = !!(opts.taskVersionCurrent || opts.taskVersionNext || opts.taskVersionInfo ||
+                             opts.taskCiEvInfo || opts.taskVersionPreReleaseId || opts.taskChangelogPrint ||
+                             opts.taskChangelogPrintVersion);
+    //
+    // Display color banner
+    // If opts.verbose s set, then the ArgumentParser will have diplayed the banner already
+    // For stdout type tasks, then we dont display the banner or anything else for that matter.
+    //
+    if (!opts.taskModeStdOut && !opts.verbose && useBanner) {
+        displayIntro(banner);
+    }
+
+    return opts;
+}
+
+
+function apBanner(version: string)
+{
+    return `                                       _      _       _
+  _ _ __ _ __   _ __      _ __  _   __| |_ | (_)_____| |  ____  ____
+ / _\\' || '_ \\\\| '_ \\\\___| '_ \\\\| \\ \\ |  _\\| | || ___| \\_/ _ \\\\/  _|
+ | (_| || |_) || |_) |___| |_) || |_| | |_)| | | \\\\__| __ | __/| |
+ \\__\\\\__| | .//| | .//   | | .//|____/|___/|_|_|/___/|_| \\___|.|_| v${version}
+        |_|    |_|       |_|`;
+}
+
+
+function displayIntro(banner: string)
+{
+    console.log(chalk.bold(gradient("cyan", "pink").multiline(banner, {interpolation: "hsv"})));
+}
+
