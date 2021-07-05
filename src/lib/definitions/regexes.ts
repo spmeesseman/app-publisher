fc60c3a40649f731966e0e64201c83bbb5330929
Scott Meesseman <spmeesseman@gmail.com>
2021-07-05 00:54:24 -0400

    progress: touch last feature check-in for pre-release changelog handling [skip ci]

--- src/lib/definitions/regexes.ts
+++ src/lib/definitions/regexes.ts
@@ -6,11 +6,11 @@
 const REGEX_CHANGELOG_MD_MSG_TICKET_TAGS = /\[(&nbsp;| )*(bugs?|issues?|closed?s?|fixe?d?s?|resolved?s?|refs?|references?){1}(&nbsp;| )*#[0-9]+((&nbsp;| )*,(&nbsp;| )*#[0-9]+){0,}(&nbsp;| )*\]/gi;
 
 const REGEX_CHANGELOG_TXT_VERSION_SECTION = (versionText: string) => {
-    return new RegExp(`(?:^${versionText} ([0-9a-zA-Z\-\.]{3,})[\r\n]+.+[\r\n]+[\-]{20,}[\r\n]+[\*]{20,}[^]+?(?=[\*]{20,})[\*]{20,}[\r\n]+)([^]*?)(?=^${versionText}|###END###)`, "gm");
+    return new RegExp(`(?:^${versionText} ([0-9a-zA-Z\\-\\.]{3,})[\r\n]+.+[\r\n]+[\\-]{20,}[\r\n]+[\\*]{20,}[^]+?(?=[\\*]{20,})[\\*]{20,}[\r\n]+)([^]*?)(?=^${versionText}|###END###)`, "gm");
 };
 
 const REGEX_CHANGELOG_MD_VERSION_SECTION = (versionText: string) => {
-  return new RegExp(`(?:^## ${versionText} \[{0,1}([0-9a-zA-Z\-\.]{3,})\]{0,1} {0,1}\([a-zA-Z0-9 ,:\/\.]+\)[\r\n]+)([^]*?)(?=^${versionText}|###END###)`, "gm");
+  return new RegExp(`(?:^## ${versionText} \\[([0-9a-zA-Z\\-\\.]{3,})\\] {0,1}\\([a-zA-Z0-9 ,:\\/\\.]+\\)[\r\n]+)([^]*?)(?=^${versionText}|###END###)`, "gm");
 };
 
 const regexes = {
